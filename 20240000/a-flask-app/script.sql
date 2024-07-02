19df2e1ed33d72bfedca151e5f92b23267e1f700
morenoxxD <157799779+morenoxxD@users.noreply.github.com>
2024-07-02 10:23:22 +0000

    comm5

--- 20240000/a-flask-app/script.sql
+++ 20240000/a-flask-app/script.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE superpippodb;
-USE superpippodb;
+CREATE DATABASE ufs05db;
+USE ufs05db;
 CREATE TABLE employees (
   first_name varchar(45) DEFAULT NULL,
   last_name varchar(45) DEFAULT NULL
 );
 
-INSERT INTO employees (first_name, last_name) VALUES ('luca', 'verdi');
+INSERT INTO employees (first_name, last_name) VALUES ('marco', 'booo');
